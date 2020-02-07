d65c047054a3f7c188372d8f27bc6b0d5773ea6a
Clint Ki <ki2clint@gmail.com>
2020-02-06 19:49:45 -0600

    numers

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("2" + 2);
+console.log(2 + "2");
+console.log(2 + 2);
+console.log("2" + "2");
+console.log(true + true);
+console.log(2 + true);
+
+console.log(true + true + "true" + "false");
+console.log(1 + 1 + "2" + "3");
