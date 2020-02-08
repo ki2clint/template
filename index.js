e20aa852f3d8e3a4be9133acface60f37f681a2d
Clint Ki <ki2clint@gmail.com>
2020-02-07 20:22:02 -0600

    in class stuff

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-console.log("2" + 2);
-console.log(2 + "2");
-console.log(2 + 2);
-console.log("2" + "2");
-console.log(true + true);
-console.log(2 + true);
+// create a variable called "name" that references a string
+const name = "Clint";
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Beetle";
+// create a variable called "wage" that references a number
+let wage = "wage";
+// create a variable called "age" that references a number
+let age = "3";
+// create a variable called "onlyChild" that references a boolean
+const onlyChild = false;
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+// create a variable called "favoriteThing" that references ANY primitive value
+let favoriteThing = "sleep";
+// print the data type of "favoriteThing"
+console.log(`${favoriteThing}`);
+// ! You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "Heaven";
+// ! You no longer like the type of data stored in "favoriteThing"
 
-console.log(true + true + "true" + "false");
-console.log(1 + 1 + "2" + "3");
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = "more sleep";
+console.log(typeof favoriteThing);
+// ! Happy Birthday!
+// Reassign "age" to a new value
+age = 24;
+// ! You got a big raise at work.
+// Reassign "wage" to a new value
+wage = "monies";
+// ! Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+// use "console.log()" and "+" to print a sentence that includes two variables
+console.log(`My name is ${name} and i am ${age}`);
